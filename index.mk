c90803c53bfbea6598e5366d4f48657e6f2a4527
Jason Tu <jasontu4@gmail.com>
2017-10-18 05:24:24 -0400

    Update

--- index.mk
+++ index.mk
@@ -1,14 +1,24 @@
 
+# Clean up build.
+clean:
+	@mix clean
+.PHONY: clean
+
 # Run tests.
-test: flow
+test:
 	@mix test
 .PHONY: test
+
+# Run linter.
+lint:
+	@mix credo list --strict
+.PHONY: lint
 
 # Run type check.
 #
 # You should install https://github.com/jeremyjh/dialyxir globally
 # before running.
-flow:
+flow: test
 	@mix dialyzer
 .PHONY: flow
 
@@ -18,3 +28,10 @@ flow:
 publish:
 	@mix publish
 .PHONY: publish
+
+# Spin up an iex session.
+#
+# The iex session runs in the context of your project.
+console:
+	@iex -S mix
+.PHONY: console
